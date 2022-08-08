42015e0a957eb643832d6996a7c821374d93527c
YSakir639 <103224018+YSakir639@users.noreply.github.com>
2022-08-08 14:16:17 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,1 +1,13 @@
+import cv2
+cap = cv2.VideoCapture("rtsp://192.168.1.1/MJPG")
+width = 980
+height = 520
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (width,height))
+    cv2.imshow('Video Stream', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
 
